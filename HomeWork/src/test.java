c838dc359b64ca5fb698d55fb2bd2d7688a274b2
pgwstr <2712278005@qq.com>
2022-08-28 22:37:58 +0800

    HomeWork

--- HomeWork/src/test.java
+++ HomeWork/src/test.java
@@ -12,12 +12,13 @@ public class test {
         System.out.println("test");
         System.out.println("123");
     }
+
     /**
      * @author pgwstr
      * @date 2022/8/18 11:21 PM
      */
-    
-  static   class Person{
+
+    static class Person {
         private int i;
         private int age;
         private static int height;
